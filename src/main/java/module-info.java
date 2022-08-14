44beabd788f2a88c4fd9154e480d68b109dc64cf
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2022-08-14 08:44:26 +0200

    remove mudle exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,21 +1,11 @@
 module org.fl.gedcomtools {
-	exports org.fl.gedcomtools.profession;
-	exports org.fl.gedcomtools.gui;
-	exports org.fl.gedcomtools.util;
-	exports org.fl.gedcomtools.line;
-	exports org.fl.gedcomtools.entity;
-	exports org.fl.gedcomtools.filtre;
-	exports org.fl.gedcomtools.io;
-	exports org.fl.gedcomtools.sosa;
-	exports org.fl.gedcomtools.sourcetype;
-	exports org.fl.gedcomtools;
 
-	requires transitive org.fl.util;
+	requires org.fl.util;
 	requires com.google.gson;
-	requires transitive java.desktop;
-	requires transitive java.logging;
-	requires transitive javafx.graphics;
-	requires transitive javafx.controls;
+	requires java.desktop;
+	requires java.logging;
+	requires javafx.graphics;
+	requires javafx.controls;
 	requires javafx.swing;
 	requires jdk.jsobject;
 }
