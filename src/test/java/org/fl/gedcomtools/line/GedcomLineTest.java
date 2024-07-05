e1c69dce7e489bfaa27dc13ce1bf00f863cde027
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2024-07-05 15:05:30 +0200

    Change Assertions static import

--- src/test/java/org/fl/gedcomtools/line/GedcomLineTest.java
+++ src/test/java/org/fl/gedcomtools/line/GedcomLineTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2023 Frederic Lefevre
+Copyright (c) 2017, 2024 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class GedcomLineTest {
 
