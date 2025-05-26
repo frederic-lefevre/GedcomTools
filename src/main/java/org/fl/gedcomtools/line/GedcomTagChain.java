ab4ee306563de8ad5593b67cdf282b7f91d1180f
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2025-05-26 18:12:27 +0200

    Format code

--- src/main/java/org/fl/gedcomtools/line/GedcomTagChain.java
+++ src/main/java/org/fl/gedcomtools/line/GedcomTagChain.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2023 Frederic Lefevre
+Copyright (c) 2017, 2025 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -32,44 +32,44 @@ public class GedcomTagChain extends LinkedList<GedcomTagValue> {
 	private static final long serialVersionUID = 1L;
 
 	public GedcomTagChain(List<GedcomTagValue> l) {
-		super(l) ;
+		super(l);
 	}
 
 	public GedcomTagChain() {
-		super() ;
+		super();
 	}
 
 	public static GedcomTagChain buildTagChain(GedcomTagChain currentTagChain, GedcomTagValue tagValue, int tagLev) {
-		
+
 		// make a copy of the current tag chain
-		GedcomTagChain newTagChain = new GedcomTagChain(currentTagChain) ;
-		
-		int levelDifference = currentTagChain.size() - tagLev ;
+		GedcomTagChain newTagChain = new GedcomTagChain(currentTagChain);
+
+		int levelDifference = currentTagChain.size() - tagLev;
 		if (levelDifference < 0) {
 			// error in the level which is too high
-			
-			return null ;
-			
-		}  else {
-			
-			newTagChain.removeNFirst(levelDifference) ;
-			newTagChain.addFirst(tagValue) ;
-			return newTagChain ;
-		}
-	}
-	
-	private void removeNFirst(int n) {
-		for (int i=0; i < n; i++) {
-			removeFirst() ;
-		}
-	}
-	
-	public boolean tagForLevelEquals(int level, GedcomTagValue tagValue) {
-		
-		if ((level < size()) && (level > -1)) {
-			return get(size() - level - 1).equals(tagValue) ;
+
+			return null;
+
 		} else {
-			return false ;
+
+			newTagChain.removeNFirst(levelDifference);
+			newTagChain.addFirst(tagValue);
+			return newTagChain;
+		}
+	}
+
+	private void removeNFirst(int n) {
+		for (int i = 0; i < n; i++) {
+			removeFirst();
+		}
+	}
+
+	public boolean tagForLevelEquals(int level, GedcomTagValue tagValue) {
+
+		if ((level < size()) && (level > -1)) {
+			return get(size() - level - 1).equals(tagValue);
+		} else {
+			return false;
 		}
 	}
 }
